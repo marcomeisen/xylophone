45539ee0c3772516ff367ba37978f27c1a207d3b
Marco Meisen <marcomeisen@gmail.com>
2019-08-14 13:08:22 +0200

    Buttons implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,70 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
+          child: Column(
+            children: <Widget>[
+
+                FlatButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  color: Colors.yellow,
+                  child: null,
+                ),
+
+              FlatButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
+                  playSound(2);
                 },
-                child: Text('Click me')),
+                color: Colors.green,
+                child: null,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                color: Colors.blue,
+                child: null,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                color: Colors.deepPurple,
+                child: null,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                color: Colors.redAccent,
+                child: null,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                color: Colors.red,
+                child: null,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                color: Colors.orangeAccent,
+                child: null,
+              ),
+            ],
           ),
         ),
       ),
